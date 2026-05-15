2d8c355c6c536f8a684d38a36fc6866d3ef19600
UnusualFrog <forwardnoah@gmail.com>
2026-05-15 20:36:22 -0230

    Home page bio progress

--- app/page.js
+++ app/page.js
@@ -1,31 +1,70 @@
 import './globals.css'; 
 import Image from 'next/image';
+import localFont from 'next/font/local'
 
+// Import Local Font
+const myFont = localFont({
+  src: '../font/cmu.typewriter-text-regular.ttf',
+})
+
+const bio_text_style = 'bio-text  ' + myFont.className
+const bio_header_style = 'bio-header  ' + myFont.className
+const pronoun_btn_style = 'pronoun-button ' + myFont.className
 
 export default function Home() {
    return (
         <div className='h-500'>
           <div className='container-home'>
+            
+            {/* Tower Contents */}
             <div className='tower-home'>
-              <div className='tower-home-shadow'>
-              </div>
+
+              {/* Shadow */}
+              <div className='tower-home-shadow'></div>
+
+              {/* Actual Content */}
               <div className='tower-home-content-base'>
-                <div className='home-content'>
-                  <div className='home-content-top'></div>
-                  {/* <div className='home-content-top'>
-                    <p>test</p><p>test</p><p>test</p>
-                  </div> */}
-                </div>
-                
-                <Image
+
+                <div className='bio-base'>
+
+                  <div className='bio-content'>
+                    
+                    <div className="bio-pfp-col">
+                      <Image
                         src="/pfp.png"
                         width={1920}
                         height={1080}
                         alt="Picture of the author"
                         className="pfp-home"
                         loading="eager"
-                ></Image>
+                      ></Image>
+                      <button disabled={true} className={pronoun_btn_style}>♀️ - She/Her</button>
+                    </div>
+                    
+                    <div className="bio-text-container">
+                      <p className={bio_header_style}>=======BIO=======</p>
+                      <p className={bio_text_style}>
+                              Welcome to my Site! My name is Julia. This site is my creative outlet. I'm a programmer and I like to dabble in digital art & game dev. Checkout my blog for thoughts on birds, frogs, and other less important things.
+                      </p>
+                    </div>
+                    
+
+                  </div>
+
+                  <div className='bio-links'>
+
+                  </div>
+
+                </div>
+
+                <div className='blinkies-base'>
+
+                </div>
+                
+                
+                
               </div>
+
             </div>
 
           </div>
