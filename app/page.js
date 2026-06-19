5b3f32fd17ed4eacfb574ebbe643852c41aec2ec
UnusualFrog <forwardnoah@gmail.com>
2026-06-19 19:05:48 -0230

    feat: Improved styling of home bio

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ const myFont = localFont({
 
 const bio_text_style = 'bio-text  ' + myFont.className
 const bio_header_style = 'bio-header  ' + myFont.className
-const pronoun_btn_style = 'pronoun-button ' + myFont.className
+const pronoun_btn_style = 'pronoun-button-home ' + myFont.className
 
 export default function Home() {
    return (
@@ -42,7 +42,7 @@ export default function Home() {
                     </div>
                     
                     <div className="bio-text-container">
-                      <p className={bio_header_style}>=======BIO=======</p>
+                      <p className={bio_header_style}>==========BIO==========</p>
                       <p className={bio_text_style}>
                               Welcome to my Site! My name is Julia. This site is my creative outlet. I am a programmer and I like to dabble in digital art & game dev. Checkout my blog for thoughts on birds, frogs, and other less important things.
                       </p>
