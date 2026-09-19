62fa95a49c7f2d237c52d900361da8fd9b1086e0
UnusualFrog <forwardnoah@gmail.com>
2026-09-19 18:02:08 -0230

    fix: Fixed images in RSS feed

--- utils/rss.js
+++ utils/rss.js
@@ -4,36 +4,39 @@ import RSS from "rss";
 // Code based on https://dev.to/promathieuthiry/creating-an-rss-feed-in-your-nextjs-project-20em
 
 export default async function generateRssFeed(allPosts) {
-  const site_url =
-    process.env.NODE_ENV === "production"
-      ? "https://nameofwebsite"
-      : "http://localhost:3000/blog";
+    const site_url =
+        process.env.NODE_ENV === "production"
+            ? "https://swamp-paradise-website.vercel.app"
+            : "http://localhost:3000";
 
-  const feedOptions = {
-    title: "Swamp Paradise blog posts | RSS Feed",
-    description: "Welcome to Swamp Paradise!",
-    site_url: site_url,
-    feed_url: `${site_url}/rss.xml`,
-    image_url: `${site_url}/pfp.png`,
-    pubDate: new Date(),
-    copyright: `All rights reserved ${new Date().getFullYear()}`,
-  };
+    const feedOptions = {
+        title: "Swamp Paradise blog posts | RSS Feed",
+        description: "Welcome to Swamp Paradise!",
+        site_url: site_url,
+        feed_url: `${site_url}/rss.xml`,
+        image_url: `${site_url}/pfp.png`,
+        pubDate: new Date(),
+        copyright: `All rights reserved ${new Date().getFullYear()}`,
+    };
 
-  const feed = new RSS(feedOptions);
+    const feed = new RSS(feedOptions);
 
-  // Add each individual post to the feed.
-  allPosts.map((post) => {
-    console.log(post.date_posted)
-    feed.item({
-      title: "Untitled Post at: " + post.date_posted,
-      description: post.main_content,
-      url: `${site_url}`,
-    //   url: `${site_url}/posts/${post.slug}`,
-      date: post.date_posted,
-      enclosure: {url:post.img, }
+    // Add each individual post to the feed.
+    allPosts.map((post) => {
+        console.log(post.date_posted)
+        feed.item({
+            title: "Untitled Post at: " + post.date_posted,
+            description: post.main_content,
+            url: `${site_url}`,
+            //   url: `${site_url}/posts/${post.slug}`,
+            date: post.date_posted,
+            enclosure: {
+                url: post.img,
+                // type: 'image/jpeg'
+            }
+        });
     });
-  });
 
-  // Write the RSS feed to a file as XML.
-  fs.writeFileSync("./public/rss.xml", feed.xml({ indent: true }));
+    // Write the RSS feed to a file as XML.
+    fs.writeFileSync("./public/rss.xml", feed.xml({ indent: true }));
 }
